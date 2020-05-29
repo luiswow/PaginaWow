4decbfebf109b6b0168146517e9413a5a564b5dd
Luis Reyes <luisjavier_iza@hotmail.com>
2020-05-28 21:20:29 -0600

    se agregaron mas elementos en levear

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    ) { }
 
   ngOnInit(): void {
   }
+  
 
 }
--- src/app/leveo/leveo.component.ts
+++ src/app/leveo/leveo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-leveo',
   templateUrl: './leveo.component.html',
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeveoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer
+    
+    ) { }
+    
+  
+    
 
-  ngOnInit(): void {
+    urlSinProcesar = "//www.youtube.com/embed/BCr7y4SLhck?rel=0";
+    //urlSinProcesar = "//www.youtube.com/embed/8pC5VZM2h8k?rel=0"+1;<--tambien los he visto de esta forma o cualquier entero
+    urlSaneada = this.sanitizer.bypassSecurityTrustResourceUrl(this.urlSinProcesar);
+
+
+    urlSinProcesar2 = "//www.youtube.com/embed/jr2JB8DSENM?rel=0";
+    //urlSinProcesar = "//www.youtube.com/embed/8pC5VZM2h8k?rel=0"+1;<--tambien los he visto de esta forma o cualquier entero
+    urlSaneadas = this.sanitizer.bypassSecurityTrustResourceUrl(this.urlSinProcesar2);
+
+
+    urlSinProcesar3 = "//www.youtube.com/embed/PWqGpq0-yh0?rel=0";
+    //urlSinProcesar = "//www.youtube.com/embed/8pC5VZM2h8k?rel=0"+1;<--tambien los he visto de esta forma o cualquier entero
+    urlSaneada3 = this.sanitizer.bypassSecurityTrustResourceUrl(this.urlSinProcesar3);
+
+
+    urlSinProcesar4 = "//www.youtube.com/embed/jgkAVRwPV9I?rel=0";
+    //urlSinProcesar = "//www.youtube.com/embed/8pC5VZM2h8k?rel=0"+1;<--tambien los he visto de esta forma o cualquier entero
+    urlSaneada4 = this.sanitizer.bypassSecurityTrustResourceUrl(this.urlSinProcesar4);
+
+
+    urlSinProcesar5 = "//www.youtube.com/embed/3DJzixGrGe4?rel=0";
+    //urlSinProcesar = "//www.youtube.com/embed/8pC5VZM2h8k?rel=0"+1;<--tambien los he visto de esta forma o cualquier entero
+    urlSaneada5= this.sanitizer.bypassSecurityTrustResourceUrl(this.urlSinProcesar5);
+    
+
+  ngOnInit(
+    
+  ): void {
+  
+    
   }
+
+
 
 }
